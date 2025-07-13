28a9f2e01209e947b1406808f0c2dbb30ffb3efd
Anna Filippova <153564718+sfc-gh-annafilippova@users.noreply.github.com>
2025-07-12 18:05:50 -0700

    add functions dir to path

--- site/build.sh
+++ site/build.sh
@@ -2,8 +2,11 @@
 
 echo "hello"
 
+export PATH=$PATH:/opt/build/repo/site/functions
+
 echo $PATH
 
 GOBIN=$(pwd)/functions go install github.com/googlecodelabs/tools/claat@latest
+
 
 npm run serve
