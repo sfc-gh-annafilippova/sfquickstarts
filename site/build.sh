66354f71da6ceb2b0b1b309e67e6620b07b7669e
Anna Filippova <153564718+sfc-gh-annafilippova@users.noreply.github.com>
2025-07-12 17:07:48 -0700

    add go to path

--- site/build.sh
+++ site/build.sh
@@ -2,6 +2,10 @@
 
 go install github.com/googlecodelabs/tools/claat@latest
 
+#adding Golang to path
+export PATH=$PATH:/usr/local/go/bin
+export PATH=$PATH:$HOME/go/bin
+
 npm install 
 
 npm run serve
