0a6843d2f370fa310a7faa7a57173326f35a76f9
Anna Filippova <153564718+sfc-gh-annafilippova@users.noreply.github.com>
2025-07-12 22:05:25 -0700

    debug args

--- site/tasks/helpers/claat.js
+++ site/tasks/helpers/claat.js
@@ -15,6 +15,9 @@ const spawn = childprocess.spawn;
 //
 exports.run = (cwd, cmd, env, fmt, ga, o, prefix, args, callback) => {
   args.unshift(cmd, '-e', env, '-f', fmt, '-ga', ga, '-o', o, '-prefix', prefix);
+
+  console.log('Args...', args)
+  
   const proc = spawn('claat', args, { stdio: 'inherit', cwd: cwd, env: process.env, shell: true });
 
   proc.on('close', (e) => {
