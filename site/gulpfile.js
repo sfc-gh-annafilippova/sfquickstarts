2cba21b1dcc4c70e63a839118f49b68ec5e324a6
Anna Filippova <153564718+sfc-gh-annafilippova@users.noreply.github.com>
2025-07-12 23:53:18 -0700

    debug source and build dirs

--- site/gulpfile.js
+++ site/gulpfile.js
@@ -137,15 +137,23 @@ gulp.task('copy:codelabs', () => {
 gulp.task('export:codelabs', (callback) => {
 
   const source = args.source;
+  
+  console.log('Source dir...', CODELABS_SRC_DIR)
+  console.log('Codelabs env...', CODELABS_ENVIRONMENT)
+  console.log('Build dir', CODELABS_BUILD_DIR)
 
-  console.log('Printing source...', source)
 
   if (source !== undefined) {
+    
     const sources = Array.isArray(source) ? source : [source];
-    claat.run(CODELABS_SRC_DIR, 'export', CODELABS_ENVIRONMENT, CODELABS_FORMAT, DEFAULT_GA, sources, callback);
+    // claat.run(CODELABS_SRC_DIR, 'export', CODELABS_ENVIRONMENT, CODELABS_FORMAT, DEFAULT_GA, "../../"+CODELABS_BUILD_DIR, sources, callback);
+    claat.run(CODELABS_SRC_DIR, 'export', CODELABS_ENVIRONMENT, CODELABS_FORMAT, DEFAULT_GA, "../../" + CODELABS_BUILD_DIR, CODELABS_ELEMENTS_PREFIX, sources, callback);
+
   } else {
+
     const sources = ["[^_]*/*.md"]; //export all markdown files in the src directory, except _imports
-    claat.run(CODELABS_SRC_DIR, 'update', CODELABS_ENVIRONMENT, CODELABS_FORMAT, DEFAULT_GA, sources, callback);
+    // claat.run(CODELABS_SRC_DIR, 'export', CODELABS_ENVIRONMENT, CODELABS_FORMAT, DEFAULT_GA, "../../"+CODELABS_BUILD_DIR, sources, callback);
+    claat.run(CODELABS_SRC_DIR, 'export', CODELABS_ENVIRONMENT, CODELABS_FORMAT, DEFAULT_GA, CODELABS_BUILD_DIR, CODELABS_ELEMENTS_PREFIX, sources, callback);
   }
   
 });
