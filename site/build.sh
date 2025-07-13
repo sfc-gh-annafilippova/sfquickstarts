5404f8f578fa292fb554d3ae0cc6dff7ae153fd7
Anna Filippova <153564718+sfc-gh-annafilippova@users.noreply.github.com>
2025-07-12 18:12:22 -0700

    add debugging for build script

--- site/build.sh
+++ site/build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-echo "hello"
-
+echo "Add functions dir to build path..."
 export PATH=$PATH:/opt/build/repo/site/functions
-
 echo $PATH
+
+# install claat in functions dir
 
 GOBIN=$(pwd)/functions go install github.com/googlecodelabs/tools/claat@latest
 
+echo "Check install in correct directory..."
+echo $(ls /opt/build/repo/site/functions)
 
 npm run serve
